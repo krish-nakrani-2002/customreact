6336d38282ad4dd745556b9b9e878cd45d2e1cb1
krish-nakrani-2002 <nakranikrish5@gmail.com>
2024-12-08 16:45:23 +0530

    for loop

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -1,9 +1,18 @@
 function customRender(reactElement, container){
+    /*
     const domElement = document.createElement(reactElement.type)
     domElement.innerHTML = reactElement.children 
     domElement.setAttribute('href', reactElement.props.href)
     domElement.setAttribute('target', reactElement.props.target)
 
+    container.appendChild(domElement)
+    */
+
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    for (const prop in reactElement.props) {
+        domElement.setAttribute(prop, reactElement.props[prop])  
+        }
     container.appendChild(domElement)
 }
 
@@ -11,10 +20,10 @@ function customRender(reactElement, container){
 const reactElement = {
     type: 'a',
     props: {
-        href: 'https://google.com',
+        href: 'https://youtube.com',
         target: '_blank'
     },
-    children: 'click me to visit google'
+    children: 'click me to visit youtube'
 }
 
 const mainContainer = document.querySelector('#root')
